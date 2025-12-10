616f623c48be896c802f5c48d0358a5d8ca4f24d
Juan Carlos Acosta Perabá <juancarlos.ap.dev@gmail.com>
2025-12-10 09:55:18 +0000

    Enhance config.form.php: Add comprehensive licensing information, improve code comments for clarity, and ensure proper handling of configuration updates with improved message formatting.

--- front/config.form.php
+++ front/config.form.php
@@ -3,6 +3,32 @@
 /**
  * -------------------------------------------------------------------------
  * Kanban Looks Good plugin for GLPI
+ * Configuration form handler
+ * -------------------------------------------------------------------------
+ *
+ * LICENSE
+ *
+ * This file is part of Kanban Looks Good.
+ *
+ * Kanban Looks Good is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Kanban Looks Good is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Kanban Looks Good. If not, see <http://www.gnu.org/licenses/>.
+ * -------------------------------------------------------------------------
+ *
+ * @package   KanbanLooksGood
+ * @author    Juan Carlos Acosta Perabá
+ * @copyright Copyright (C) 2024-2025 by Juan Carlos Acosta Perabá
+ * @license   GPLv3+ http://www.gnu.org/licenses/gpl.html
+ * @link      https://github.com/JuanCarlosAcostaPeraba/kanbanlooksgood
  * -------------------------------------------------------------------------
  */
 
@@ -11,22 +37,35 @@ if (!defined('GLPI_ROOT')) {
     include('../../../inc/includes.php');
 }
 
+// Verify user has permission to update configuration
 Session::checkRight('config', UPDATE);
 
+// Handle configuration update submission
 if (isset($_POST['update_config'])) {
     $result = PluginKanbanlooksgoodConfig::saveConfig($_POST);
+    
     if ($result) {
-        Session::addMessageAfterRedirect(__('Configuration saved successfully', 'kanbanlooksgood'), false, INFO);
+        Session::addMessageAfterRedirect(
+            __('Configuration saved successfully', 'kanbanlooksgood'),
+            false,
+            INFO
+        );
     } else {
-        Session::addMessageAfterRedirect(__('Error saving configuration', 'kanbanlooksgood'), false, ERROR);
+        Session::addMessageAfterRedirect(
+            __('Error saving configuration', 'kanbanlooksgood'),
+            false,
+            ERROR
+        );
     }
+    
     Html::back();
 } else {
+    // Display configuration form
     Html::header(
         __('Kanban Looks Good', 'kanbanlooksgood'),
         $_SERVER['PHP_SELF'],
-        "config",
-        "plugins"
+        'config',
+        'plugins'
     );
 
     PluginKanbanlooksgoodConfig::showConfigForm();
